4169cdbc0912c8c9de87f9576f47ccbae6781c40
Oscar Riojas <oscarriojas@gmail.com>
2025-01-26 21:38:26 -0500

    feat/ include hash transactions

--- arbisepolia/src/main.rs
+++ arbisepolia/src/main.rs
@@ -1,9 +1,10 @@
-use reqwest::blocking::Client;
-use serde_json::Value;
-use std::env;
 use std::fs;
-use std::path::Path;
+use std::env;
 use dotenv::dotenv;
+use std::path::Path;
+use serde::Deserialize;
+use serde_json::Value;
+use reqwest::blocking::Client;
 
 struct ArbiSepScanner {
     key: String,
@@ -11,20 +12,10 @@ struct ArbiSepScanner {
 }
 
 impl ArbiSepScanner {
-    /// Creates a new instance of `ArbiSepScanner` with the given API key and URL.
     fn new(key: String, url: String) -> Self {
         ArbiSepScanner { key, url }
     }
 
-    /// Gets the transaction list for a given address.
-    ///
-    /// # Arguments
-    ///
-    /// * `address` - The address of the account to query.
-    ///
-    /// # Return
-    ///
-    /// A `Result` containing a `Value` representing the transaction list, or an error if the request fails.
     fn transaction_address(&self, address: &str) -> Result<Value, reqwest::Error> {
         let url = format!(
             "{}?module=account&action=txlist&address={}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey={}",
@@ -34,38 +25,73 @@ impl ArbiSepScanner {
         let response = Client::new().get(&url).send()?.json()?;
         Ok(response)
     }
+
+    fn transaction_hash(&self, hash: &str) -> Result<Value, reqwest::Error> {
+        let url = format!(
+            "{}?module=account&action=txlistinternal&txhash={}&apikey={}",
+            self.url, hash, self.key
+        );
+
+        let response = Client::new().get(&url).send()?.json()?;
+        Ok(response)
+    }
 }
 
-/// Downloads all transactions from a specific contract address
-/// and saves them to a JSON file at data/response.json.
+#[derive(Deserialize)]
+struct Transaction {
+    hash: String,
+    // Otros campos que necesites
+}
+
+#[derive(Deserialize)]
+struct Response {
+    result: Vec<Transaction>,
+}
+
 fn main() {
     dotenv().ok();
 
     let key = env::var("KEY").expect("KEY not set");
-    let url = env::var("ARBITRUM_URL").expect("SEPOLIA_URL not set");
-    let address_contract = "0x8c5ff04497062be94e59412163a2e771a8154beb";
+    let url = env::var("ARBITRUM_URL").expect("ARBITRUM_URL not set");
+    let address_contract = "0x346Ac3698f6a1Ed8B78C9594284406A4506d0d68";
 
     let scanner = ArbiSepScanner::new(key, url);
 
-    // Get transactions from a specific address
+    // Obtener transacciones de una dirección específica
     match scanner.transaction_address(address_contract) {
         Ok(response) => {
-            // Create the data directory if it doesn't exist
+            // Crear el directorio de datos si no existe
             let path = Path::new("data");
             if !path.exists() {
                 fs::create_dir(path).expect("Failed to create data directory");
             }
 
-            // Write the response to a JSON file
+            // Escribir la respuesta en un archivo JSON
             let file_path = path.join(format!("{}.json", address_contract));
             fs::write(&file_path, serde_json::to_string_pretty(&response).expect("Failed to serialize JSON"))
                 .expect("Failed to write to file");
 
-            // Print the file name
+            // Imprimir el nombre del archivo
             println!(
                 "Response saved to {}",
                 file_path.to_str().expect("Failed to convert path to string")
             );
+
+            // Leer el archivo JSON
+            let data = fs::read_to_string(file_path).expect("Unable to read file");
+
+            // Deserializar el JSON a la estructura Response
+            let response: Response = serde_json::from_str(&data).expect("JSON was not well-formatted");
+
+            // Iterar sobre los resultados y consultar cada hash
+            for transaction in response.result {
+                match scanner.transaction_hash(&transaction.hash) {
+                    Ok(response) => {
+                        println!("Response for hash {}: {}", transaction.hash, serde_json::to_string_pretty(&response).expect("Failed to serialize JSON"));
+                    }
+                    Err(e) => eprintln!("Error for hash {}: {}", transaction.hash, e),
+                }
+            }
         }
         Err(e) => eprintln!("Error: {}", e),
     }
